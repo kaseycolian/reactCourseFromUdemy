523258afbc3aa1b0785606e5eeab606fec9edef5
Kasey Colian <kaseycolian@gmail.com>
2018-07-21 12:43:29 -0400

    Switched to ES6.  Replaced forEach w/ Map(). Added If statement in map()

--- src/App.js
+++ src/App.js
@@ -6,23 +6,29 @@ class App extends Component {
   componentDidMount() {
     console.log('mounted')
   }
-
-  consoleLog() {
+  //Removing this method to keep app compact and just write method in component.
+  //This is a simple method and can be directly made in component
+  /*consoleLog() {
     console.log('I\'m pressed!');
-  }
+  }*/
+
   //method part of Component Class
   render() {
 
     var word = 'hello';
     var style = {fontSize: '100px'};
     
-    var array = ['welcome', 'to', 'my', 'beginner', 'react', 'project'];
+    const array = ['welcome', 'to', 'my', 'beginner', 'react', 'project'];
+    array.push('from', 'Udemy');
 
-    var arrayToRender = [];
-    array.forEach(function(word){
-      arrayToRender.push(
-        <p>{word}</p>)
-    })
+    //For Loops: Key Props are required, it will render, but throw error in console
+    //Unique Keys are required for handling & storing w/ React
+    //in this situation, each word will be different, so we can use {word}
+    // const arrayToRender = [];
+    // array.forEach(function(word){
+    //   arrayToRender.push(
+    //     <p key={word}>{word}</p>)
+    // })
 
     //variables get added w/ {}
     //components get added with <>
@@ -30,10 +36,21 @@ class App extends Component {
     return (
       <div>
         <p className = "large" style={{fontSize: "200px"}}>meow</p>
-        <p style ={style}>{word}</p>        
-        {arrayToRender}
+        { /*This was where we called in original forEach loop, but was replaced w/ map */
+        /*<p style ={style}>{word}</p> */}    
+       
+        {array.map(word => {
+           if (word !== 'from' && word !== 'Udemy') {
+            return (
+               <p key={word}>{word}</p>
+              )
+          }
+        })}
+      
         <NameComponent/>
-        <button onClick={ this.consoleLog }>
+       {//Removing this called in component and writing directly into onClick
+       /* <button onClick={ this.consoleLog }>*/}
+          <button onClick = {() => { console.log('I am clicked!') }}>
             <NameComponent/>
         </button>
       </div>
