22a712f0d9cb525738298f8e64917502b5cba03f
Kasey Colian <kaseycolian@gmail.com>
2018-07-24 13:56:23 -0400

    End of Lecture 18. Added react-select, react-bootstrap nav bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import NameComponent from './Components/NameComponent'
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
+import Select from 'react-select';
 
 class App extends Component {
   //comes before components, helps set up fundamental for everything else
   constructor(props) {
     super(props);
-    //can bind handleClick() to state here as well as when calling function
-    // this.handleClick = this.handleClick.bind(this);
     this.state = {
-      user_name: 'Kasey',
-      profession: 'Developer'
+      selectedOption: null,
     };
   }
   //happens once a setState call happens (ie: button click in this code)
@@ -21,34 +19,62 @@ class App extends Component {
   componentDidMount() {
     console.log('mounted')
   }
-  
-  handleClick() {
-    this.setState ({
-      user_name: "Kasey R Colian",
-      profession: "Future Sr Developer"
-    });
+
+  //needs to be binded to effect state b/c it's a function not inherent to Component Class
+  //binded in onChange in render()
+  handleChange(selectedOption) {
+    this.setState({ selectedOption });   
+    console.log(`Option selected:`, selectedOption);
   }
 
-  //method part of Component Class
   render() {
+    const { selectedOption } = this.state;
 
-    //one way to reference array in state is to declare a variable instead of calling this.state.array directly
-    // const { array } = this.state;
-
-    const style = {color: 'purple', fontSize: '100px'};
-    
     return (
-      <div className = "header">
-        <p style={style}>
-          {this.state.user_name} - {this.state.profession}
-        </p>
+      <div>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">KC's Page</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">
+              Home
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Link
+            </NavItem>
+            <NavDropdown eventKey={3} title="Data Views" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>List</MenuItem>
+              <MenuItem eventKey={3.2}>Search</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.4}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>        
+        <div className = "container">
+          <div className = "row">
+            <div className = "col-sm-12">
+              <h1>Kasey's Page</h1>
+              <p>Here we'll list some data from a bunch of sources!</p>
+               <Select
+                  placeholder = "Select a number..."
+                  isMulti = "true"
 
-        <NameComponent user_name={this.state.user_name}/>
-
-        <button onClick = {this.handleClick.bind(this)}>
-            <NameComponent/>
-        </button>
-
+                  name="form-field-name"
+                  value={selectedOption}
+                /* this is where we bind the created function to effect state*/
+                  onChange={this.handleChange.bind(this)}
+                  options={[
+                    {value: 'one', label: 'One'},
+                    {value: 'two', label: 'Two'},
+                  ]}
+                />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
